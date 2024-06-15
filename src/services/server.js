d784052fc267b1457fee6b9dd24d74402f7884a1
MihirP01 <mihirjpatel@hotmail.co.uk>
2024-06-15 04:47:57 +0100

    Add PostgreSQL integration

--- src/services/server.js
+++ src/services/server.js
@@ -1,36 +1,23 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const Product = require("../models/Product.js");
+const { Client } = require("pg");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Replace with your MongoDB connection string
-const mongoUri =
-  "mongodb+srv://Cluster73735:d1xFTGN+cVN8@cluster73735.ey7o6pg.mongodb.net"; // Or your Atlas connection string
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-app.use(express.json());
-
-app.post("/api/products", async (req, res) => {
-  const { name, thumbnail, materialCost } = req.body;
-  const product = new Product({ name, thumbnail, materialCost });
-  try {
-    await product.save();
-    res.status(201).json(product);
-  } catch (error) {
-    res.status(500).json({ error: "Error saving product" });
-  }
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.json(products);
-  } catch (error) {
-    res.status(500).json({ error: "Error fetching products" });
-  }
+client.connect();
+
+app.get("/", async (req, res) => {
+  const result = await client.query("SELECT NOW()");
+  res.send(`Hello, Heroku! The current time is: ${result.rows[0].now}`);
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
