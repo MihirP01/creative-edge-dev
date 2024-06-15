d11e010321d1b882eec15640cc4181ff7539e1d9
MihirP01 <mihirjpatel@hotmail.co.uk>
2024-06-15 04:58:42 +0100

    updated heroku

--- src/services/server.js
+++ src/services/server.js
@@ -3,6 +3,7 @@ const { Client } = require("pg");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const HOST = "0.0.0.0";
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -14,10 +15,15 @@ const client = new Client({
 client.connect();
 
 app.get("/", async (req, res) => {
-  const result = await client.query("SELECT NOW()");
-  res.send(`Hello, Heroku! The current time is: ${result.rows[0].now}`);
+  try {
+    const result = await client.query("SELECT NOW()");
+    res.send(`Hello, Heroku! The current time is: ${result.rows[0].now}`);
+  } catch (err) {
+    console.error(err);
+    res.send("Error: Unable to fetch current time from the database.");
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
